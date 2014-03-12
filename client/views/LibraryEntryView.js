a93a50f3954155e7825354bc284a7d9752ddb921
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2014-03-12 16:43:11 -0700

    Added buttons, song queue in progress

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,10 +3,10 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td><button class="play">Play</button></td><td><button class="queue">Add to Queue</button></td>'),
 
   events: {
-    'click': function() {
+    'click .play': function() {
       this.model.play();
     }
   },
